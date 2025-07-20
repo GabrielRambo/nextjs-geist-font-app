27561b1ac993f7d1b9445bf6dd18c80687deb2fe
Gabriel Rambo Anschau <151236235+GabrielRambo@users.noreply.github.com>
2025-07-20 10:37:18 -0300

    Add .next/static/chunks/src_app_login_page_tsx_a993527f._.js 1753018584015

---
+++ .next/static/chunks/src_app_login_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_757294fe._.js",
+  "static/chunks/src_a5928fc7._.js"
+],
+    source: "dynamic"
+});
